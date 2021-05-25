6b4b60fac49cd72a1209ae71639226276f5ab536
MarcioSill <marcio.sill@hotmail.com>
2021-05-24 21:01:02 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -15,8 +15,13 @@ public class Main extends Application {
 		try {
 			//estancia FXMLLoader loader passando o caminho da view
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load(); // carrega a view
-			Scene mainScene = new Scene(parent); // estancia a sena principal passando como argumento parent
+			ScrollPane scrollPane = loader.load(); // carrega a view
+			
+			//para encaixe de janela, a janela encaixa no tamanho do paco
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scrollPane); // estancia a sena principal passando como argumento parent
 			primaryStage.setScene(mainScene); // seta o pauco como sena principal
 			primaryStage.setTitle("Sample JavaFX appication"); // titulo do pauco
 			primaryStage.show();
